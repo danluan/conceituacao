d3ae4ab7fbe6522f60b759b5f8d017c07c6469cc
danluan <danielluanlourencol@gmail.com>
2025-06-29 15:55:10 -0300

    feat: implementação login e logout com token

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -4,31 +4,63 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    public function login(Request $request){
-        return 'login';
-    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required',
+        ]);
 
-    public function register(Request $request){
-        $fields = $request->validate([
-                'name'=> 'required|max:255',
-                'email'=> 'required|email|unique:users',
-                'password'=> 'required|confirmed'
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['As credenciais estão incorretas.'],
             ]);
+        }
 
-            $user = User::create($fields);
+        $user->tokens()->delete();
 
-            $token = $user->createToken($request->name);
+        $token = $user->createToken($user->name)->plainTextToken;
 
-            return [
-                'user' => $user,
-                'token'=> $token->plainTextToken
-            ];
+        return response([
+            'user' => $user,
+            'token_type' => 'Bearer',
+            'access_token' => $token
+        ], 200);
+
     }
 
-    public function logout(Request $request){
-        return 'logout';
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed'
+        ]);
+
+        $user = User::create($fields);
+
+        $token = $user->createToken($request->name);
+
+        return response([
+            'user' => $user,
+            'token_type' => 'Bearer',
+            'access_token' => $token
+        ], 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response([
+            'message' => 'Usuário desconectado.'
+        ], 200);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,10 +8,6 @@ use App\Http\Controllers\AuthController;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get("/", function () {
-    return 'api';
-});
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
